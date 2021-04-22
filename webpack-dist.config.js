3790e537046554da8c3d563766acec9cbf278287
SunnyQjm <qjm253@pku.edu.cn>
2021-04-22 17:36:24 +0800

    modify: 修改简历

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -53,14 +53,14 @@ module.exports = {
     }),
     new OptimizeCSSAssetsPlugin(),
     new EndWebpackPlugin(async () => {
-      // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.qjm253.cn');
-
-      // 调用 Chrome 渲染出 PDF 文件
-      const chromePath = findChrome();
-      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'https://resume.qjm253.cn' // 这里注意改成你的在线简历的网站
-      ]);
+      // // 自定义域名
+      // fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.qjm253.cn');
+      //
+      // // 调用 Chrome 渲染出 PDF 文件
+      // const chromePath = findChrome();
+      // spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
+      //   'https://resume.qjm253.cn' // 这里注意改成你的在线简历的网站
+      // ]);
     }),
   ]
 };
