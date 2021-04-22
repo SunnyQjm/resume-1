e8494d043fbb3857bb06f65f88116173a8dca5ab
SunnyQjm <qjm253@pku.edu.cn>
2021-04-22 17:00:18 +0800

    modify: 修改简历

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -59,7 +59,7 @@ module.exports = {
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://resume.qjm253.cn' // 这里注意改成你的在线简历的网站
+        'https://resume.qjm253.cn' // 这里注意改成你的在线简历的网站
       ]);
     }),
   ]
